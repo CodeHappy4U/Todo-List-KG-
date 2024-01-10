e29a161e343832c190f87c7a93fdac92ca090a82
CodeHappy4U <happypatel9825@gmail.com>
2024-01-10 15:14:01 +0530

    33. Update AddTodo component to use refs instead of state for todoName and dueDate.

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,26 +1,36 @@
-import { useState,useRef } from "react";
+import { useRef } from "react";
 import { MdOutlineAddComment } from "react-icons/md";
 
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const noOfUpdates = useRef(0);
+  // const [todoName, setTodoName] = useState("");
+  // const [dueDate, setDueDate] = useState(""); //not updating state require
 
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-    noOfUpdates.current += 1;
-  };
+  // CREATED REFERENCE USE REF
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-    console.log(`noOfUpdates are :${noOfUpdates.current}`);
-  };
+  // Not require
+  // const handleNameChange = (event) => {
+    //   setTodoName(event.target.value);
+    //   noOfUpdates.current += 1;
+    // };
+    
+  // Not require
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  //   console.log(`noOfUpdates are :${noOfUpdates.current}`);
+  // };
 
   const handleAddButtonClicked = (event) => {
     event.preventDefault(); //default to pass on server
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value="";
+    dueDateElement.current.value="";
+    // console.log(`${todoName} due on: ${dueDate}`)
     onNewItem(todoName, dueDate);
-    setDueDate("");
-    setTodoName("");
+    // setDueDate("");
+    // setTodoName("");
   };
 
   return (
@@ -29,13 +39,14 @@ function AddTodo({ onNewItem }) {
         <div className="col-6">
           <input
             type="text"
+            ref={todoNameElement}
             placeholder="Enter Todo Here"
-            value={todoName}
-            onChange={handleNameChange}
+            // value={todoName}
+            // onChange={handleNameChange}
           />
         </div>
         <div className="col-4">
-          <input type="date" value={dueDate} onChange={handleDateChange} />
+          <input type="date" ref={dueDateElement}   />
         </div>
         <div className="col-2">
           <button
